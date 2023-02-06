52310cbd2cbb2cfd18cb9998f361e1cbfff2f863
gonsolisc2698 <gonsolisc@gmail.com>
2023-02-06 13:51:44 +0100

    Update ejercicio_esri.js

--- javascript/ejercicio_esri.js
+++ javascript/ejercicio_esri.js
@@ -1,4 +1,12 @@
 const publicacion = "esri";
-publicacion[0].toUpperCase();
-const publicacion = "environmental", "systems", "research", "institute";
-publicacion[0].toUpperCase();
+publicacion[0].toUpperCase() + publication.substring(1);
+const publicacion = "environmental", 
+publication[0].toUpperCase() + publication.substring(1);
+const publicacion = "systems",
+publication[0].toUpperCase() + publication.substring(1);
+const publicacion = "research",
+publication[0].toUpperCase() + publication.substring(1);
+const publicacion = "systems",
+publication[0].toUpperCase() + publication.substring(1);
+const publicacion = "institute",
+publication[0].toUpperCase() + publication.substring(1);
