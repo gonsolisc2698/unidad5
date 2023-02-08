9562a471728253ea91bfa555e8587f672fd24dc2
gonsolisc2698 <gonsolisc@gmail.com>
2023-02-08 15:43:29 +0100

    Create ejerciciomayuscula.js

---
+++ javascript/ejerciciomayuscula.js
@@ -0,0 +1,6 @@
+const topics = ["JavaScript", "Variables", "Funciones", "Condicionales", "Bucles"];
+const upperCaseTopics = topics.map(function(topic) {
+    return topic.toUpperCase(); });
+const reverseTopics = upperCaseTopics.reverse();
+console.log('los tópicos  de antes eran '+topics)
+console.log('los tópicos nuevos son'+reverseTopics);
