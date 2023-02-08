1057b0d19bb2b0a214274e3de424131487d0b7d7
gonsolisc2698 <gonsolisc@gmail.com>
2023-02-08 17:36:20 +0100

    Create ejercicio12.js

---
+++ javascript/ejercicio12.js
@@ -0,0 +1,11 @@
+class Square{
+        constructor(side){
+            this.perimeter= side*4;
+            this.area = side*side;
+
+        };
+};
+
+const pequeño = new Square(2);
+
+console.log(pequeño);
