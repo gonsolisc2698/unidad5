70f76943c5522d1fa493392ba15759bd1050133d
gonsolisc2698 <gonsolisc@gmail.com>
2023-02-08 17:18:52 +0100

    Update ejerciciomayuscula.js

--- javascript/ejerciciomayuscula.js
+++ javascript/ejerciciomayuscula.js
@@ -3,4 +3,4 @@ const upperCaseTopics = topics.map(function(topic) {
     return topic.toUpperCase(); });
 const reverseTopics = upperCaseTopics.reverse();
 console.log('los tópicos  de antes eran '+topics)
-console.log('los tópicos nuevos son'+reverseTopics);
+console.log('los tópicos nuevos son '+reverseTopics);
