7199cf433e1266a5495a8bd2d2acda603e640906
gonsolisc2698 <gonsolisc@gmail.com>
2023-02-14 17:27:12 +0100

    cambio nombre

---
+++ EjerciciosHTML/ejercicioselect.js
@@ -0,0 +1,4 @@
+function cambioCiudad(){
+    var x = document.getElementById('cities').value;
+    document.getElementById("cities-select").innerHTML= 'La ciudad seleccionada es '+ x;
+}
---
+++ EjerciciosHTML/opinion.js
@@ -0,0 +1,19 @@
+function capturartexto{(
+    var texto = document.getElementById("opinion").value;
+    document.getElementById("rellenar").innerHTML = opinion;
+
+)
+}
+//controle lo que escriba el usario
+var textarea = document.getElementById('opinion');
+textarea.addEventListener('keyup', ckeckenter);
+
+
+//controlar solo cuando usa el enter keycode= enter (13)
+function ckeckenter(evt){
+    if (evt.keyCode===13){
+        capturartexto();
+    }
+}
+
+//llamar a la función que pinta en  pantalla - capturar texto()
