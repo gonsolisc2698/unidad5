48c9d617536eed472631a9ab669767fb23461f57
gonsolisc2698 <gonsolisc@gmail.com>
2023-02-14 23:33:33 +0100

    edicion

--- EjerciciosHTML/ejercicalcular.js
+++ EjerciciosHTML/ejercicalcular.js
@@ -1,22 +1,48 @@
-var resultadoSuma = 0;
-var resultadomedia = 0;
-
-
-function calcular(){
-    var nums = [3, 4, 5];
-//  Sumar
-   for(let i=0; i<nums.length; i++) {
-        resultadoSuma += nums[i]
-        console.log('1', resultadoSuma)
-   }
-   for (let i=0; i<nums.length; i++){
-        resultadomedia === nums[i/nums.length]
-        console.log('1', resultadomedia)
-   }
-
-   document.getElementById('resultSuma').innerHTML = 'El resultado de la suma es ' + resultadoSuma
-   console.log(resultadoSuma)
-   document.getElementById('resultmedia').innerHTML='El resultado de la media es '+ resultadomedia
-   console.log(resultadomedia)
-//    document.write('el resultado de la suma es '+resultadoSuma)
-}
+function calcular() {
+     var numerosInput = document.getElementById('inputNumbers').value.split(",");
+     console.log('numerosInput', numerosInput);
+     var numeros = numerosInput.map(function(number) {
+         return parseInt(number);
+     });
+     // Función flecha
+     // var numeros = numerosInput.map((number) => parseInt(number));
+     
+     console.log('numeros', numeros);
+     // Suma
+     var suma = 0;
+     for (let i = 0; i < numeros.length; i++ ) {
+         suma += numeros[i]; 
+     }
+     document.getElementById('resultadoSuma').innerHTML = 'El resultado de la suma es ' + suma;
+     // Media
+     var media = (suma / numeros.length).toFixed(2);
+     document.getElementById('resultadoMedia').innerHTML = 'El resultado de la media es ' + media;
+     // Máximo
+     // var max = Math.max(...numeros)
+     // console.log(max)
+     var max = numeros[0]; 
+     for (let i = 0; i < numeros.length; i++) {
+         if (max < numeros[i]) {
+             max = numeros[i];
+         } 
+     }
+     document.getElementById('resultadoMax').innerHTML = 'El mayor valor es ' + max;
+     // Mínimo
+     var min = numeros[0]; 
+     for (let i = 0; i < numeros.length; i++) {
+         if (min > numeros[i]) {
+             min = numeros[i];
+         } 
+     }
+     document.getElementById('resultadoMin').innerHTML = 'El mínimo valor es ' + min;
+ }
+ function reiniciar() {
+     document.getElementById('inputNumbers').value = '';
+     document.getElementById('resultadoSuma').innerHTML = '';
+     document.getElementById('resultadoMedia').innerHTML = '';
+     document.getElementById('resultadoMax').innerHTML = '';
+     document.getElementById('resultadoMin').innerHTML = '';
+ }
+ function cargar(){
+     document.getElementById('inputNumbers').value = "1,2,3"
+ }
--- EjerciciosHTML/ejerciciolista.js
+++ EjerciciosHTML/ejerciciolista.js
@@ -1,1 +1,27 @@
-
+function addTask() {
+    // Captura lo que dice el usuario
+    var tarea = document.getElementById('taskInput').value;
+    console.log('tare', tarea);
+    
+    // Mostrarlo por pantalla
+    // var etiquetaP = document.createElement('p');
+    // // Opt1
+    // etiquetaP.innerHTML = tarea;
+    // // Opt2
+    // etiquetaP.append(tarea);
+    // document.body.append(etiquetaP);
+    // Mostrar tareas en forma de lista
+    var tareaLi = document.createElement('li');
+    // tareaLi.innerHTML = tarea;
+    var check = document.createElement('input');
+    // Opt 1
+    // check.type = 'checkbox';
+    // Opt 2
+    check.setAttribute('type', 'checkbox');
+    tareaLi.appendChild(check)
+    tareaLi.append(tarea)
+    
+    // Rellenarla con tareas (li)
+    var listaTarea = document.getElementById('listaTareas'); // Elemento ul del HTML
+    listaTarea.appendChild(tareaLi);
+}
