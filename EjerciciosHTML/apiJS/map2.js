aff7bc56a3960e145d73e4d0cd1c4436197d10b1
gonsolisc2698 <gonsolisc@gmail.com>
2023-02-20 19:53:17 +0100

    r67uopñko9p87654eww

--- EjerciciosHTML/apiJS/map2.js
+++ EjerciciosHTML/apiJS/map2.js
@@ -4,10 +4,15 @@ require(
     "esri/layers/FeatureLayer",
     "esri/geometry/Extent",
     "esri/SpatialReference",
+    "esri/dijit/HomeButton",
+    "esri/dijit/BasemapToggle",
+    "esri/dijit/OverviewMap",
+    "esri/dijit/Legend",
+    "dojo/on",
     "dojo/domReady!"],
     function (Map,
         ArcGISDynamicMapServiceLayer, 
-        FeatureLayer, Extent, SpatialReference,){
+        FeatureLayer, Extent, SpatialReference, HomeButton, BasemapToggle, OverviewMap, Legend){
         var myMap = new Map('viewDiv', {
             basemap: 'satellite',
             extent: new Extent(-122.68,45.53,-122.45,45.60,
@@ -24,7 +29,36 @@ require(
         //myMap.addLayer(USA);
         //myMap.addLayer(Terremotos);
         myMap.addLayers([USA,Terremotos]);
+        var casa = new HomeButton({
+            map: myMap
+        }, "homeDiv");
+        casa.startup();
 
-    }
 
-    );
+        var Toggle = new BasemapToggle({
+
+            map: myMap,
+            visible: true,
+            basemap: "dark-gray"
+          }, "toggleDiv");
+        Toggle.startup();
+
+        var OverView = new OverviewMap({
+            map: myMap,
+            attachTo: "bottom-right",
+            visible:true
+        });
+        OverView.startup();
+
+        function displayLeyenda() {
+           console.log('Capas Cargadas')
+           var leyenda =  new Legend({
+            map: myMap
+           }, 'leyendaDiv');
+           leyenda.startup();
+    
+        };
+        myMap.on('layers-add-result', displayLeyenda);
+
+        
+    });
